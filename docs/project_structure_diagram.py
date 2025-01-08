4416b950164c61431b02d81042e91d925e5e561e
MikloCO <37591806+MikloCO@users.noreply.github.com>
2025-01-08 20:54:26 +0100

    Rough sketch of game engine

--- docs/project_structure_diagram.py
+++ docs/project_structure_diagram.py
@@ -1,31 +1,55 @@
 from diagrams import Diagram, Cluster
-from diagrams.aws.compute import EC2
-from diagrams.aws.network import ELB
-from diagrams.aws.database import RDS
+from diagrams.aws.compute import Lambda
+from diagrams.aws.ml import Sagemaker
+from diagrams.aws.storage import S3
+from diagrams.generic.network import Firewall
+from diagrams.generic.os import Windows
 from diagrams.programming.language import Python
-from diagrams.programming import flowchart
+from diagrams.azure.network import PublicIpAddresses
+from diagrams.programming.language import C
+from diagrams.elastic.elasticsearch import Monitoring
+
+
 import os
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
-with Diagram("Game engine", show=False, filename="docs/architecture") as diag:
 
-    with Cluster("Components"):
-        scene = flowchart.Display("Scene graph")
-        game_manager = flowchart.Action("Game manager")
-        state_synchronization = flowchart.Action("State synchronization")
-        main = Python("main")
+with Diagram("Game Engine Architecture", show=False, direction="TB", filename="docs/architecture") as diag:
 
+    # Render Engine
+    with Cluster("Render Engine"):
+        render = Python("Rendering Engine")
+        graphics = C("Graphics: Vulkan")
+        audio = S3("Audio")
+        ui = PublicIpAddresses("UI")
+
+        render >> [graphics, audio, ui]
+
+    # Resource Handling
+    with Cluster("Resource Handling"):
+        state_synchronization = Monitoring("Game Loop")
+        resources = S3("Asset Management")
+        scene = S3("Scene Graph")
+        artworks = S3("Animations")
+        game_objects = S3("Game Objects")
+
+        state_synchronization >> [resources, scene, artworks, game_objects]
+
+    # Physics
+    with Cluster("Physics"):
+        math = Sagemaker("Math")
+        physics = Firewall("Physics Engine")
+        collision = Firewall("Collision Handler")
+
+
+    # Gameplay
     with Cluster("Gameplay"):
-        with Cluster("Scripting"):
-            scripting = flowchart.Display("Scripting")
-            game_objects = flowchart.Display("Game objects")
-        with Cluster("Art"):
-            artworks = flowchart.Display("Animations")
-            audio = flowchart.Display("Audio")
+        scripting = Python("Scripting")
+        npc = Lambda("NPC Logic")
+        input_io = PublicIpAddresses("Input System")
 
-
-    state_synchronization >> scripting
+        scripting >> [npc, input_io]
 
 
 
-diag.render()
+    diag.render()
